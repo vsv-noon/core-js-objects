f70f63c53b8c259bd1aa45dd4159139a293db947
vsv-noon <forvsn@gmail.com>
2025-12-17 14:32:50 +0700

    feat: solved new tasks

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -201,8 +201,13 @@ function sellTickets(queue) {
  *    console.log(r.height);      // => 20
  *    console.log(r.getArea());   // => 200
  */
-function Rectangle(/* width, height */) {
-  throw new Error('Not implemented');
+function Rectangle(width, height) {
+  this.width = width;
+  this.height = height;
+
+  Rectangle.prototype.getArea = function getArea() {
+    return this.width * this.height;
+  };
 }
 
 /**
@@ -215,8 +220,8 @@ function Rectangle(/* width, height */) {
  *    [1,2,3]   =>  '[1,2,3]'
  *    { height: 10, width: 20 } => '{"height":10,"width":20}'
  */
-function getJSON(/* obj */) {
-  throw new Error('Not implemented');
+function getJSON(obj) {
+  return JSON.stringify(obj);
 }
 
 /**
